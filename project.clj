210cbaad78ead287b4ab6596ea06b09213bae700
mihird <mihir.deshpande@helpshift.com>
2024-05-27 14:58:23 +0530

    Fix project.clj file

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject clj-deepwalk "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure library to provide core functionalities for graph embedding"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
