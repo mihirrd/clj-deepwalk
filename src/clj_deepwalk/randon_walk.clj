8d4c71f2f7a1dacd9c4e03887eef751f22821242
mihird <mihir.deshpande@helpshift.com>
2024-05-27 14:51:30 +0530

    Add functions to generate random walks

---
+++ src/clj_deepwalk/randon_walk.clj
@@ -0,0 +1,59 @@
+(ns clj-deepwalk.randon-walk
+  (:require [loom.graph :as lg]
+            [loom.io :as lio]
+            [clojure.java.io :as io]))
+
+(defn random-walk
+  "Generates a random walk in the graph starting from `start-node` of length
+  `walk-length`."
+  [graph start-node walk-length]
+  (loop [current-node start-node
+         path [start-node]
+         remaining-steps (dec walk-length)]
+    (if (zero? remaining-steps)
+      path
+      (let [neighbors (lg/successors graph current-node)]
+        (if (empty? neighbors)
+          path ;; End the walk if no neighbors are available
+          (let [next-node (rand-nth (vec neighbors))]
+            (recur next-node (conj path next-node) (dec remaining-steps))))))))
+
+
+(defn generate-random-walks
+  "Given a `graph`, `num-walks` (number of walks) to be generated per node and `walk-length`,
+  returns an array of random walks from each node in the graph"
+  [graph num-walks walk-length]
+  (let [nodes (lg/nodes graph)]
+    (mapcat (fn [node]
+              (repeatedly num-walks #(random-walk graph node walk-length)))
+            nodes)))
+
+
+(defn generate-pairs
+  "Generates context-target pairs from random walks."
+  [walks window-size]
+  (mapcat
+    (fn [walk]
+      (for [i (range (count walk))
+            j (range (max 0 (- i window-size))
+                     (min (count walk) (+ i window-size 1)))
+            :when (not= i j)]
+        [(nth walk i) (nth walk j)]))
+    walks))
+
+
+(defn deepwalk
+  "Main function to run DeepWalk algorithm on a given graph and return the trained model."
+  [graph num-walks walk-length vector-size window-size epochs]
+  (let [walks (generate-random-walks graph num-walks walk-length)
+        pairs (generate-pairs walks window-size)]
+    (train-skipgram pairs vector-size window-size epochs)))
+
+
+(defn get-embeddings
+  "Extracts embeddings for each node from the trained model."
+  [model nodes]
+  (reduce (fn [embeddings node]
+            (assoc embeddings node (.getWordVector model (str node))))
+          {}
+          nodes))
