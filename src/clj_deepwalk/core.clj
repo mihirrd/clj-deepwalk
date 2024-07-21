0c7a367814db9e6aa2b96e36bb093ee6148e3cb6
mihird <mihir.deshpande@helpshift.com>
2024-07-21 13:45:46 +0530

    Add deepwalk main function

--- src/clj_deepwalk/core.clj
+++ src/clj_deepwalk/core.clj
@@ -1,1 +1,17 @@
-(ns clj-deepwalk.core)
+(ns clj-deepwalk.core
+  (:require [graphs.core :as gc]
+            [model.core :as mc]))
+
+
+(defn deepwalk
+  "Run DeepWalk algorithm on the given graph"
+  {:opts [:vector-size
+          :walk-length
+          :num-walks
+          :window-size
+          :learning-rate]}
+  [edges & opts]
+  (let [graph (gc/create-graph edges)
+        trained-vectors (mc/train-deepwalk graph (apply hash-map opts))]
+    {:graph graph
+     :vectors trained-vectors}))
