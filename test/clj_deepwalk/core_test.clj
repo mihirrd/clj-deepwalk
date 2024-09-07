00cfa4946eccd4477b12153a1cb2a264de3d30a3
mihird <mihir.deshpande@helpshift.com>
2024-09-07 18:15:36 +0530

    Add test for create-graphs utility

--- test/clj_deepwalk/core_test.clj
+++ test/clj_deepwalk/core_test.clj
@@ -1,7 +1,19 @@
 (ns clj-deepwalk.core-test
   (:require [clojure.test :refer :all]
-            [clj-deepwalk.core :refer :all]))
+            [graphs.core :as gc]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest test-create-graph
+  (let [edges-1 [[:a :b] [:a :c] [:b :d]]
+        edges-2 [[:a :b] [:c :d]]]
+    (testing "create-graph"
+      (is (= {:a #{:b :c}
+              :b #{:a :d}
+              :c #{:a}
+              :d #{:b}}
+             (gc/create-graph edges-1)))
+      (is (= {:a #{:b}
+              :b #{:a}
+              :c #{:d}
+              :d #{:c}}
+             (gc/create-graph edges-2))))))
